90c5a0264a2f6565ee465c491c2f9dad40ac14b3
Abdalrahman <kingabdo1212120000000000@gmail.com>
2025-03-20 01:55:34 +0200

    S4

---
+++ Sheet_4/dist/Q1.js
@@ -0,0 +1,44 @@
+"use strict";
+function isPrime(n) {
+    if (n < 2)
+        return false;
+    for (let i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+function TheFinalResult() {
+    const Ele = document.getElementById("ser");
+    const btn = document.getElementById("btn");
+    const Show = document.getElementById("show");
+    if (!Ele || !Show)
+        return;
+    btn.addEventListener("click", () => {
+        Show.innerHTML = "";
+        const inpuNumber = Ele.value.split("-");
+        const ArrayOfnumber = [];
+        inpuNumber.forEach(i => {
+            if (!isNaN(parseInt(i)))
+                ArrayOfnumber.push(parseInt(i));
+        });
+        const PrimeNumber = ArrayOfnumber.find(e => isPrime(e));
+        if (PrimeNumber !== undefined) {
+            console.log(`Founded`);
+        }
+        else {
+            console.log(`Not Founded any Prime Number`);
+        }
+        const ShowThePrimeNumber = ArrayOfnumber.filter(e => isPrime(e));
+        ShowThePrimeNumber.forEach(i => {
+            const r = document.createElement("tr");
+            const c = document.createElement("td");
+            c.textContent = i.toString();
+            r.appendChild(c);
+            Show.appendChild(r);
+        });
+    });
+}
+TheFinalResult();
+//# sourceMappingURL=Q1.js.map
---
+++ Sheet_4/src/Q1.ts
@@ -0,0 +1,47 @@
+//Q1
+function isPrime(n:number) : boolean {
+    if(n < 2) return false;
+    for(let i =2 ; i * i <= n; i++) {
+        if(n%i==0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function TheFinalResult() : void{
+    const Ele = document.getElementById("ser") as HTMLInputElement;
+    const btn = document.getElementById("btn") as HTMLButtonElement;
+    const Show = document.getElementById("show") as HTMLTableElement;
+    if(!Ele || !Show) return;
+
+    btn.addEventListener("click" , ()=>{
+        Show.innerHTML ="";
+        const inpuNumber = Ele.value.split("-");
+        const ArrayOfnumber :number[] = [];
+        inpuNumber.forEach(i=>{
+            if(!isNaN(parseInt(i))) ArrayOfnumber.push(parseInt(i));
+        })
+
+        const PrimeNumber = ArrayOfnumber.find(e=>isPrime(e));
+        
+        if(PrimeNumber!==undefined) {
+            console.log(`Founded`);
+        }
+        else {
+            console.log(`Not Founded any Prime Number`);
+        }
+
+        
+        const ShowThePrimeNumber = ArrayOfnumber.filter(e=>isPrime(e));
+        
+        ShowThePrimeNumber.forEach(i=>{
+            const r = document.createElement("tr");
+            const c = document.createElement("td");
+            c.textContent = i.toString();
+            r.appendChild(c);
+            Show.appendChild(r);
+        });
+    });
+}
+TheFinalResult();
